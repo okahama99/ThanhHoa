ca462449b6ac053e7d84f2822510783493a12fd2
Okahama <48874863+Okahama@users.noreply.github.com>
2023-07-07 20:17:51 +0700

    Test ImageController 2

--- src/main/java/com/example/thanhhoa/controllers/ImageController.java
+++ src/main/java/com/example/thanhhoa/controllers/ImageController.java
@@ -2,6 +2,7 @@ package com.example.thanhhoa.controllers;
 
 import com.example.thanhhoa.services.image.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,12 +21,12 @@ public class ImageController {
     @Autowired
     private ImageService imageService;
 
-    @PostMapping(value = "/convertFromFileToImageURL", produces = "application/json;charset=UTF-8")
+    @PostMapping(value = "/convertFromFileToImageURL", produces = "application/json;charset=UTF-8", consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
     public String convertFromFileToImageURL(@RequestPart(name = "file") MultipartFile file) throws Exception {
         return imageService.convertFileToImageURL(file);
     }
 
-    @PutMapping(value = "/addImage", produces = "application/json;charset=UTF-8")
+    @PutMapping(value = "/addImage", produces = "application/json;charset=UTF-8", consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
     public String addImage(@RequestParam String entityName,
                            @RequestParam String entityID,
                            @RequestPart(name = "file") MultipartFile file) throws Exception {
