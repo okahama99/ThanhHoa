e538820b8a07c5276f21083da455c8c495a9a648
Okahama <48874863+Okahama@users.noreply.github.com>
2023-08-15 21:58:25 +0700

    Set serviceID in CreateServiceTypeModel into nullable

--- src/main/java/com/example/thanhhoa/dtos/ServiceTypeModels/CreateServiceTypeModel.java
+++ src/main/java/com/example/thanhhoa/dtos/ServiceTypeModels/CreateServiceTypeModel.java
@@ -3,6 +3,7 @@ package com.example.thanhhoa.dtos.ServiceTypeModels;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.annotation.Nullable;
 import java.io.Serializable;
 
 @Getter
@@ -12,5 +13,6 @@ public class CreateServiceTypeModel implements Serializable {
     private String size;
     private String unit;
     private Integer percentage;
+    @Nullable
     private String serviceID;
 }
